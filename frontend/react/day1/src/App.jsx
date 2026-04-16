4ed8f5c4d38150da4ed17dd911ea9e9dc74f95d4
danyboyell <ndhanesh670@gmail.com>
2026-04-17 03:30:46 +0530

    react components

---
+++ frontend/react/day1/src/App.jsx
@@ -0,0 +1,20 @@
+import Banner from './components/Banner'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
+
+
+const  App = () => {
+  return (<>
+  
+  <h1>this is app</h1>
+  <Navbar/>
+  <Banner/>
+  <Card/>
+  
+  
+  
+  </>)
+  
+}
+
+export default App
---
+++ frontend/react/day1/src/components/Card.jsx
@@ -0,0 +1,8 @@
+const Card = () => {
+    return(<>
+    
+    <h1>CARD</h1>
+    
+    </>)
+}
+export default Card
